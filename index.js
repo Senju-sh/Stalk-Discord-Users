3d118867f193f4d11d8b2dea3556190e2379e863
Senju <90538064+Senju-sh@users.noreply.github.com>
2024-09-24 02:45:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client } = require('ssj-v13.js'), fetch = require('node-fetch'), fs = require('fs'), senju = new Client({ ws: { properties: { os: 'Linux', browser: 'Discord Client', release_channel: 'stable', client_version: '1.0.9011', os_version: '10.0.22621', os_arch: 'x64', system_locale: 'en-US', client_build_number: 175517, native_build_number: 29584, client_event_source: null, design_id: 0, } } }), config = require("./config.json")
+const { Client } = require('ssj-v13.js'), fetch = require('node-fetch'), fs = require('node:fs'), senju = new Client({ ws: { properties: { os: 'Linux', browser: 'Discord Client', release_channel: 'stable', client_version: '1.0.9011', os_version: '10.0.22621', os_arch: 'x64', system_locale: 'en-US', client_build_number: 175517, native_build_number: 29584, client_event_source: null, design_id: 0, } } }), config = require("./config.json")
 let lastAvatar = null, lastBanner = null, lastStatus = null;
 
 senju.on('ready', () => {
